b26f027a72b8411362a59d21470fab63875f42dd
Tristan Mckone <tristanmckone@gmail.com>
2021-11-15 20:41:04 -0500

    vector2d class updates

--- src/ca/durhamcollege/Vector2D.java
+++ src/ca/durhamcollege/Vector2D.java
@@ -1,5 +1,7 @@
 package ca.durhamcollege;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Vector2D
 {
     // PRIVATE INSTANCE VARIABLES
@@ -29,7 +31,7 @@ public class Vector2D
     {
         this.y = y;
     }
-    public void Set(float x, float y)
+    public void set(float x, float y)
     {
         this.x = x;
         this.y = y;
@@ -37,10 +39,43 @@ public class Vector2D
     }
 
     // CONSTRUCTORS
+    Vector2D()
+    {
+        this.x =0;
+        this.y =0;
+    }
+    Vector2D(float x, float y)
+    {
+        set(x, y);
+    }
+
+    Vector2D(@NotNull Vector2D vector)
+    {
+        set(vector.getX(), vector.getY());
+    }
+
 
     // PRIVATE METHODS
 
     // PUBLIC METHODS
+    public void add(Vector2D rhs)
+    {
+        this.setX(this.getX() + rhs.getX());
+        this.setY(this.getY() + rhs.getY());
+    }
+
+    public void subtract(Vector2D rhs)
+    {
+        this.setX(this.getX() - rhs.getX());
+        this.setY(this.getY() - rhs.getY());
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "(" + x + ", " + y + ")";
+    }
 
     // STATIC METHODS
 
