d64b0d57f9b6367a33501d6146630c504dca67ec
Tristan Mckone <tristanmckone@gmail.com>
2021-11-15 20:09:34 -0500

    vector2d class

---
+++ src/ca/durhamcollege/Vector2D.java
@@ -0,0 +1,48 @@
+package ca.durhamcollege;
+
+public class Vector2D
+{
+    // PRIVATE INSTANCE VARIABLES
+    private float x;
+    private float y;
+
+
+
+
+    // PUBLIC PROPERTIES
+    public float getX()
+    {
+        return x;
+    }
+
+    public void setX(float x)
+    {
+        this.x = x;
+    }
+
+    public float getY()
+    {
+        return y;
+    }
+
+    public void setY(float y)
+    {
+        this.y = y;
+    }
+    public void Set(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    // CONSTRUCTORS
+
+    // PRIVATE METHODS
+
+    // PUBLIC METHODS
+
+    // STATIC METHODS
+
+
+}
